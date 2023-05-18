2b0a8ffebb2bc6ef7434e31c2acc88f386ed5277
GeoTzoum <94116573+GeoTzoum@users.noreply.github.com>
2023-05-19 00:47:14 +0300

    Delete SQL FIles directory

--- SQL FIles/6a.sql
+++
@@ -1,5 +0,0 @@
--- Ερώτημα 6α
-
-SELECT * FROM transactions WHERE store_id = $store_id and date_of_transaction between $start_date and $end_date and 
-payment_methods_id =$payment_method and number_of_products > $number_of_products
-and total_cost > $total_cost;
--- SQL FIles/indexes.sql
+++
@@ -1,8 +0,0 @@
-CREATE INDEX idx_transactions_date_of_transaction
-ON supermarket.`transactions` (date_of_transaction);
-
-CREATE INDEX idx__transactions_total_cost
-ON supermarket.`transactions` (total_cost);
-
-CREATE INDEX idx_transactions_payment_method
-ON supermarket.`transactions` (payment_methods_id);
